ca79e9d5d86f7bc5d2eb13740fc0b9b119921793
Abdullah Aydoğan <114025267+abdullahaydogan@users.noreply.github.com>
2024-01-26 08:06:26 +0300

    deneme1

--- demo/src/main/java/com/example/demo/SolutionChallengeApplication.java
+++ demo/src/main/java/com/example/demo/SolutionChallengeApplication.java
@@ -8,6 +8,7 @@ public class SolutionChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolutionChallengeApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
